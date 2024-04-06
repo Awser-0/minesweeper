3d81a4c33a8133adfa8e2455c7e6d348b216c04a
awser <xk260814@163.com>
2024-04-06 21:39:05 +0800

    完善逻辑，优化代码

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 // import 'package:flutter_svg/flutter_svg.dart';
 
@@ -12,19 +14,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      // title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page1'),
+      home: const HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
-  final String title;
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -36,32 +37,202 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
+          title: const Text("Home Page 哈哈"),
         ),
-        body: Box(
-          row: 9,
-          col: 9,
-          mineCount: 10,
-        ));
+        body: GameWrapper());
   }
 }
 
-class Box extends StatefulWidget {
-  late int row;
-  late int col;
-  late int mineCount;
-  final blockSize = 30.0;
-  Box(
-      {super.key,
-      required this.col,
-      required this.row,
-      required this.mineCount});
+class GameWrapper extends StatefulWidget {
+  int row = 9;
+  int col = 9;
+  int mineCount = 10;
+  // bool isOver = false;
+  DateTime? startAt;
+  DateTime? endAt;
+  List<CellInfo> list = [];
+  GameWrapper({super.key}) {
+    reset();
+  }
+
+  void reset() {
+    list = createNewList([]);
+    startAt = null;
+    endAt = null;
+  }
+
+  void onStart(int i, int j) {
+    for (int i = 0; i < row * col; i++) {
+      list.removeAt(0);
+    }
+    list.addAll(createNewList([i * col + j]));
+    startAt = DateTime.now();
+    endAt = null;
+  }
+
+  void onEnd(bool result) {
+    if (result) {
+      debugPrint("游戏结束，你成功了！");
+    } else {
+      debugPrint("游戏结束，你失败了。。");
+    }
+    for (var item in list) {
+      if (result) {
+        item.isShow = true;
+      } else {
+        if (item.isMine) {
+          item.isShow = true;
+        }
+      }
+      item.updateUrl();
+    }
+    endAt = DateTime.now();
+  }
+
+  bool _isStart() {
+    return startAt != null;
+  }
+
+  bool _isEnd() {
+    return endAt != null;
+  }
+
+  List<CellInfo> createNewList(List<int> filterIndexs) {
+    List<CellInfo> newList = [];
+    for (int i = 0; i < mineCount; i++) {
+      newList.add(CellInfo(
+          isShow: false,
+          isFlag: false,
+          isMine: true,
+          isBlast: false,
+          count: 0));
+    }
+    for (int i = mineCount; i < row * col; i++) {
+      newList.add(CellInfo(
+          isShow: false,
+          isFlag: false,
+          isMine: false,
+          isBlast: false,
+          count: 0));
+    }
+    newList.shuffle();
+    // 过滤掉炸弹的位置
+    // 主要是限制掉第一个点开是炸弹
+    while (true) {
+      bool ok = true;
+      for (var i in filterIndexs) {
+        if (newList[i].isMine) {
+          ok = false;
+          break;
+        }
+      }
+      if (ok) {
+        break;
+      } else {
+        newList.shuffle();
+      }
+    }
+
+    for (int index = 0; index < row * col; index++) {
+      int startI = index ~/ col - 1;
+      int startJ = index % col - 1;
+      for (int i = startI; i < startI + 3; i++) {
+        for (int j = startJ; j < startJ + 3; j++) {
+          if (_isInCellArea(i, j)) {
+            if (newList[i * col + j].isMine) {
+              newList[index].count++;
+            }
+          }
+        }
+      }
+      newList[index].updateUrl();
+    }
+    return newList;
+  }
+
+  bool _isInCellArea(int i, int j) {
+    if (i < 0 || i >= row) return false;
+    if (j < 0 || j >= col) return false;
+    return true;
+  }
+
+  bool _showCell(int i, int j) {
+    int index = i * col + j;
+    if (!_isInCellArea(i, j) || list[index].isShow) return false;
+
+    list[index].isShow = true;
+    if (list[index].isMine) {
+      // 点开炸弹，游戏结束，提示信息
+      list[index].isBlast = true;
+      onEnd(false);
+    } else {
+      // 点击格子周围炸弹数为0，自动点开周围格子
+      if (list[index].count == 0) {
+        _showCell(i - 1, j - 1);
+        _showCell(i - 1, j);
+        _showCell(i - 1, j + 1);
+        _showCell(i, j - 1);
+        _showCell(i, j + 1);
+        _showCell(i + 1, j - 1);
+        _showCell(i + 1, j);
+        _showCell(i + 1, j + 1);
+      }
+    }
+    return true;
+  }
+
+  void cellTap(int i, int j) {
+    if (!_isStart()) {
+      onStart(i, j);
+    }
+    if (_isEnd() || !_showCell(i, j)) return;
+    // 检测是不是只剩炸弹
+    // 还没点开格子的数量
+    int notShowCount = 0;
+    for (var item in list) {
+      if (!item.isShow) {
+        notShowCount++;
+      }
+      item.updateUrl();
+    }
+    // 还没点开总数 小于等于 炸弹总数，游戏一定结束
+    if (notShowCount <= mineCount) {
+      onEnd(true);
+    }
+    // list[index].isShow = true;
+    // if (list[index].isMine) {
+    //   // 点开炸弹，游戏结束，提示信息
+    //   list[index].isBlast = true;
+    //   onEnd(false);
+    // } else {
+    //   // 点击格子周围炸弹数为0，自动点开周围格子
+    //   if (list[index].count == 0) {
+    //     cellTap(i - 1, j - 1);
+    //     cellTap(i - 1, j);
+    //     cellTap(i - 1, j + 1);
+    //     cellTap(i, j - 1);
+    //     cellTap(i, j + 1);
+    //     cellTap(i + 1, j - 1);
+    //     cellTap(i + 1, j);
+    //     cellTap(i + 1, j + 1);
+    //   }
+    // }
+  }
 
   @override
-  State<Box> createState() => _Box();
+  State<GameWrapper> createState() => _GameWrapperState();
 }
 
-class _Box extends State<Box> {
+class _GameWrapperState extends State<GameWrapper> {
+  // int row = 9;
+  // int col = 9;
+  // int mineCount = 10;
+  // final blockSize = 30.0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -74,44 +245,41 @@ class _Box extends State<Box> {
           margin: const EdgeInsets.all(0), //长度
           child: Column(
             children: [
-              GestureDetector(
-                onTap: () {
-                  print("笑脸点击");
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Image.asset(
-                    "assets/images/smile.png",
-                    width: 30,
-                    height: 30,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Container(
-                  width: widget.blockSize * widget.col,
-                  height: widget.blockSize * widget.row,
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                    border: Border(
-                      top: BorderSide(
-                          width: 3.0, color: Color.fromRGBO(123, 123, 123, 1)),
-                      left: BorderSide(
-                          width: 3.0, color: Color.fromRGBO(123, 123, 123, 1)),
-                      right: BorderSide(
-                          width: 3.0, color: Color.fromRGBO(255, 255, 255, 1)),
-                      bottom: BorderSide(
-                          width: 3.0, color: Color.fromRGBO(255, 255, 255, 1)),
-                    ),
-                  ),
-                  child: BlockGridView(
-                    row: widget.row,
-                    col: widget.col,
-                    mineCount: widget.mineCount,
-                  ))
+              GameInfo(smileOnTap: () {
+                widget.reset();
+                setState(() {});
+              }),
+              CellsBlock(
+                row: widget.row,
+                col: widget.col,
+                list: widget.list,
+                cellTap: widget.cellTap,
+              )
             ],
           ) //盒子边界之外的距离
           ),
+    );
+  }
+}
+
+class GameInfo extends StatelessWidget {
+  void Function()? smileOnTap;
+  GameInfo({super.key, this.smileOnTap});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        smileOnTap!();
+      },
+      child: Container(
+        padding: const EdgeInsets.all(4.0),
+        child: Image.asset(
+          "assets/images/smile.png",
+          width: 30,
+          height: 30,
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
@@ -147,148 +315,64 @@ class CellInfo {
   }
 }
 
-class BlockGridView extends StatefulWidget {
-  int row;
-  int col;
-  int mineCount;
-  bool isOver = false;
+class CellsBlock extends StatefulWidget {
+  late int row;
+  late int col;
+  // int mineCount;
+  // bool isOver = false;
   late List<CellInfo> list = [];
-  BlockGridView(
+  void Function(int i, int j) cellTap;
+  CellsBlock(
       {super.key,
       required this.row,
       required this.col,
-      required this.mineCount}) {
-    list = updateList();
-  }
-  List<CellInfo> updateList() {
-    isOver = false;
-    List<CellInfo> newList = [];
-    for (int i = 0; i < mineCount; i++) {
-      newList.add(CellInfo(
-          isShow: false,
-          isFlag: false,
-          isMine: true,
-          isBlast: false,
-          count: 0));
-    }
-    for (int i = mineCount; i < row * col; i++) {
-      newList.add(CellInfo(
-          isShow: false,
-          isFlag: false,
-          isMine: false,
-          isBlast: false,
-          count: 0));
-    }
-    newList.shuffle();
-    for (int index = 0; index < row * col; index++) {
-      int startI = index ~/ col - 1;
-      int startJ = index % col - 1;
-      for (int i = startI; i < startI + 3; i++) {
-        for (int j = startJ; j < startJ + 3; j++) {
-          if (isInCellArea(i, j)) {
-            if (newList[i * col + j].isMine) {
-              newList[index].count++;
-            }
-          }
-        }
-      }
-      newList[index].updateUrl();
-    }
-    return newList;
-  }
-
-  bool isInCellArea(int i, int j) {
-    if (i < 0 || i >= row) return false;
-    if (j < 0 || j >= col) return false;
-    return true;
-  }
-
-  void cellTap(int i, int j) {
-    if (isOver) return;
-    if (!isInCellArea(i, j)) return;
-    int index = i * col + j;
-    if (list[index].isShow) return;
-    list[index].isShow = true;
-    if (list[index].isMine) {
-      // 点开炸弹，游戏结束，提示信息
-      list[index].isBlast = true;
-      isOver = true;
-      list.forEach((item) {
-        if (item.isMine) {
-          item.isShow = true;
-        }
-      });
-      print("游戏结束");
-    } else {
-      // 点击格子周围炸弹数为0，自动点开周围格子
-      if (list[index].count == 0) {
-        cellTap(i - 1, j - 1);
-        cellTap(i - 1, j);
-        cellTap(i - 1, j + 1);
-        cellTap(i, j - 1);
-        cellTap(i, j + 1);
-        cellTap(i + 1, j - 1);
-        cellTap(i + 1, j);
-        cellTap(i + 1, j + 1);
-      }
-    }
-  }
-
-  void updateInfo() {
-    // 还没点开总数
-    int notShowCount = 0;
-    list.forEach((item) {
-      if (!item.isShow) {
-        notShowCount++;
-      }
-      item.updateUrl();
-    });
-    // 还没点开总数 小于等于 炸弹总数，游戏一定结束
-    if (notShowCount <= mineCount) {
-      isOver = true;
-    }
-  }
+      required this.cellTap,
+      // required this.mineCount,
+      required this.list});
 
   @override
-  State<BlockGridView> createState() => _BlockGridViewState();
+  State<CellsBlock> createState() => CellsBlockState();
 }
 
-class _BlockGridViewState extends State<BlockGridView> {
-  @override
-  void initState() {
-    super.initState();
-    // new Future.delayed(const Duration(seconds: 1), () {
-    //   print(" +++++++++++++++ ");
-    //   setState(() {
-    //     widget.arr = widget.updateArr();
-    //   });
-    // });
-  }
-
+class CellsBlockState extends State<CellsBlock> {
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 9,
-          crossAxisSpacing: 0,
-          mainAxisSpacing: 0,
+    return Container(
+        width: widget.col * 30,
+        height: widget.row * 30,
+        decoration: const BoxDecoration(
+          color: Colors.blue,
+          border: Border(
+            top:
+                BorderSide(width: 3.0, color: Color.fromRGBO(123, 123, 123, 1)),
+            left:
+                BorderSide(width: 3.0, color: Color.fromRGBO(123, 123, 123, 1)),
+            right:
+                BorderSide(width: 3.0, color: Color.fromRGBO(255, 255, 255, 1)),
+            bottom:
+                BorderSide(width: 3.0, color: Color.fromRGBO(255, 255, 255, 1)),
+          ),
         ),
-        itemCount: widget.row * widget.col,
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                widget.cellTap(index ~/ widget.col, index % widget.col);
-                widget.updateInfo();
-              });
-            },
-            child: Image.asset(
-              widget.list[index].url,
-              width: 30,
-              height: 30,
-              fit: BoxFit.cover,
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: widget.col,
+              crossAxisSpacing: 0,
+              mainAxisSpacing: 0,
             ),
-          );
-        });
+            itemCount: widget.row * widget.col,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  widget.cellTap(index ~/ widget.col, index % widget.col);
+                  setState(() {});
+                },
+                child: Image.asset(
+                  widget.list[index].url,
+                  width: 30,
+                  height: 30,
+                  fit: BoxFit.cover,
+                ),
+              );
+            }));
   }
 }
